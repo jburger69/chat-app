cad4f7294b5dc8126e47b3f46a02ec45f8157c60
Joshua Burger <joshberger6969@outlook.com>
2021-12-17 22:45:19 -0500

    rails g model room with migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_18_033751) do
+ActiveRecord::Schema.define(version: 2021_12_18_034256) do
+
+  create_table "rooms", force: :cascade do |t|
+    t.string "name"
+    t.boolean "is_private", default: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
